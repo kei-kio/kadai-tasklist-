0ba34c6fae7d765f9543d4541ad3bf0babaad2df
m.kei <crimson0055@gmail.com>
2019-07-06 11:15:00 +0000

    index,show,edit,deleteの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,20 +4,52 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "タスク作成完了"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクが作成できませんでした。"
+      render :new
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "タスクの更新しました。"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクは更新できませんでした。"
+      render :edit
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "タスクを削除しました。"
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
